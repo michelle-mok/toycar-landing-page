4d189a9fe2fb9aff473c3d778d45358985ab86c1
michellemok <michellemokhuihui@gmail.com>
2026-09-15 16:16:20 +0800

    chore: register gsap plugins

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { Page } from './Page.tsx';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useGSAP } from '@gsap/react';
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
